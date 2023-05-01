783942ab56792bf780df23058525037cc813cb5e
Artem <jakmobius@gmail.com>
2023-05-01 20:11:39 +0300

    Initial Commit

---
+++ Graydit/GrayditApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrayditApp.swift
+//  Graydit
+//
+//  Created by Артем on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GrayditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
